e15e2d2b744172479c0091f654a02d3a6a9ead96
Alex Kramer <alex9jk@gmail.com>
2019-10-15 09:17:57 -0400

    init

---
+++ database/migrations/2019_10_10_222230_create_games_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigInteger('player1ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
