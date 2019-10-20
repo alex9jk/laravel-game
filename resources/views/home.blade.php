6ccf7ca4bd6d9bf2a27e8b740f1ea03c7034e14c
Alex Kramer <alex9jk@gmail.com>
2019-10-20 10:49:14 -0400

    recent changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Welcome to Connect 4 Lobby</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,14 +17,14 @@
                     You are logged in! Welcome, {{$user->name}}
                     <div>
                     @foreach ($messages as $message)
-                        <div> {{ $message->messageText }}</div>
+                        <div> {{  $user->name . " " . $message->created_at .":". $message->messageText }}</div>
                     @endforeach
                     </div>
                     <div>
                         <form method="POST">
                             <input type="text" name="message" />
                             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                            <input type="submit" name="submit"/>
+                            <input type="submit" name="submit" value="send"/>
                         </form>
                     </div>
                 </div>
